51a49f940f1028c940f7e8be682b09b6c0b354c5
fede-pe <fepe.ig@gmail.com>
2019-10-31 10:10:05 +0100

    protocol interface done, track file generated and seed fidModel calculated

--- imod/protocols/protocol_fiducialModel.py
+++ imod/protocols/protocol_fiducialModel.py
@@ -44,187 +44,194 @@ class ProtFiducialModel(pyem.EMProtocol, ProtTomoBase):
         https://bio3d.colorado.edu/imod/doc/etomoTutorial.html
     """
 
-    _label = 'fiducial model generation'
+    ######################################################################CLEAN
+    commandline = "autofidseed -TrackCommandFile track.com -MinSpacing 0.85 -PeakStorageFraction 1.0 -TwoSurfaces " \
+                  "-TargetNumberOfBeads 25"
+    commandline = "autofidseed -StandardInput BB.preali -TrackCommandFile track.com -MinSpacing 0.85 " \
+                  "-PeakStorageFraction 1.0 -TwoSurfaces -TargetNumberOfBeads 25"
 
-    commandline="autofidseed -TrackCommandFile track.com -MinSpacing 0.85 -PeakStorageFraction 1.0 -TwoSurfaces -TargetNumberOfBeads 25"
-    commandline="autofidseed -StandardInput BB.preali -TrackCommandFile track.com -MinSpacing 0.85 -PeakStorageFraction 1.0 -TwoSurfaces -TargetNumberOfBeads 25"
+    missingLines = "PrealignTransformFile	BBa.prexg --------------------------"
+
+    ##########################################################################################################################
+
+    _label = 'fiducial model'
+
     # -------------------------- DEFINE param functions -----------------------
     def _defineParams(self, form):
         form.addSection('Input')
+        form.addParam('inputSetOfTiltSeries', params.PointerParam,
+                      pointerClass='SetOfTiltSeries',
+                      important=True,
+                      label='Input set of tilt-Series')
 
-        form.addParam('computeAlignment', params.EnumParam,
+        form.addParam('twoSurfaces', params.EnumParam,
+                      choices=['Yes', 'No'],
+                      default=0,
+                      label='Find on two surfaces',
+                      important=True,
+                      display=params.EnumParam.DISPLAY_HLIST,
+                      help="Track fiducials differentiating in which side of the sample are located.")
+
+        form.addParam('numberFiducial',
+                      params.IntParam,
+                      label='Number of fiducials',
+                      default='25',
+                      help="Number of fiducials to be tracked for alignment.")
+
+        form.addParam('rotationAngle',
+                      params.FloatParam,
+                      label='Tilt rotation angle (deg)',
+                      default='0.0',
+                      expertLevel=params.LEVEL_ADVANCED,
+                      help="Angle from the vertical to the tilt axis in raw images.")
+
+        form.addParam('importTrackFile', params.EnumParam,
                       choices=['Yes', 'No'],
                       default=1,
-                      label='Compute alignment', important=True,
+                      label='Import track file',
+                      important=True,
                       display=params.EnumParam.DISPLAY_HLIST,
-                      help='Compute and save the aligned tilt-series.')
+                      expertLevel=params.LEVEL_ADVANCED,
+                      help="Import a customized track file for the execution of the program.")
 
-        form.addParam('inputTiltSeries', params.PointerParam,
-                       pointerClass='TiltSeries',
-                       important=True,
-                       label='Input tilt-Series')
+        group = form.addGroup('Track file',
+                              condition='importTrackFile==0')
 
-        form.addParam('rotationAngle', params.FloatParam,
-                       label='Tilt rotation angle (deg)',
-                       help='Angle from the vertical to the tilt axis in raw '
-                            'images.')
+        group.addParam('trackFilePath',
+                       params.PathParam,
+                       label='File path',
+                       expertLevel=params.LEVEL_ADVANCED,
+                       help="Customized file path location.")
 
     # -------------------------- INSERT steps functions ---------------------
     def _insertAllSteps(self):
-        ts = self.inputTiltSeries.get()
-        tsId = ts.getTsId()
-        self._insertFunctionStep('convertInputStep', tsId)
-        if self.computeAlignment.get == 0:
-            self._insertFunctionStep('computeXcorrStep', tsId)
-        self._insertFunctionStep('createOutputStep')
+        self._insertFunctionStep('convertInputStep')
+        self._insertFunctionStep('generateTrackComStep')
+        self._insertFunctionStep('generateFiducialSeedStep')
+        self._insertFunctionStep('_createOutputStep')
 
     # --------------------------- STEPS functions ----------------------------
-    def convertInputStep(self, tsId):
-        ts = self.inputTiltSeries.get()
-        workingFolder = self._getExtraPath(tsId)
-        prefix = os.path.join(workingFolder, tsId)
-        pw.utils.makePath(workingFolder)
+    def convertInputStep(self):
+        for ts in self.inputSetOfTiltSeries.get():
+            tsId = ts.getTsId()
+            workingFolder = self._getExtraPath(tsId)
+            prefix = os.path.join(workingFolder, tsId)
+            pw.utils.makePath(workingFolder)
 
-        tiList = [ti.clone() for ti in ts]
-        tiList.sort(key=lambda ti: ti.getTiltAngle())
+            tiList = [ti.clone() for ti in ts]
+            tiList.sort(key=lambda ti: ti.getTiltAngle())
+            tiList.reverse()
 
-        writeTiStack(tiList,
-                     outputStackFn=prefix + '.st',
-                     outputTltFn=prefix + '.rawtlt')
+            writeTiStack(tiList,
+                         outputStackFn=prefix + '.st',
+                         outputTltFn=prefix + '.rawtlt')
 
-    def computeXcorrStep(self, tsId):
-        workingFolder = self._getExtraPath(tsId)
-        paramsXcorr = {
-            'input': '%s.st' % tsId,
-            'output': '%s.prexf' % tsId,
-            'tiltfile': '%s.rawtlt' % tsId,
-            'RotationAngle': 12.5,
-            'FilterSigma1': 0.03,
-            'FilterSigma2': 0.05,
-            'FilterRadius2': 0.25
-        }
-        argsXcorr = "-input %(input)s " \
-                    "-output %(output)s " \
-                    "-tiltfile %(tiltfile)s " \
-                    "-RotationAngle %(RotationAngle)f " \
-                    "-FilterSigma1 %(FilterSigma1)f " \
-                    "-FilterSigma2 %(FilterSigma2)f " \
-                    "-FilterRadius2 %(FilterRadius2)f"
-        self.runJob('tiltxcorr', argsXcorr % paramsXcorr, cwd=workingFolder)
+    def generateTrackComStep(self):
+        for ts in self.inputSetOfTiltSeries.get():
+            paramsDict = {
+                          'tsId': ts.getTsId(),
+                          'rotationAngle': self.rotationAngle.get()
+                          }
+            self.translateTrackCom(ts.getTsId(), paramsDict)
 
-        self.allocateTransformMatrix(workingFolder + '%s.prexf' % tsId, self.inputTiltSeries.get())
+    def generateFiducialSeedStep(self):
+        for ts in self.inputSetOfTiltSeries.get():
+            tsId = ts.getTsId()
+            workingFolder = self._getExtraPath(tsId)
+            paramsAutofidseed = {
+                               'trackCommandFile': '%s_track.com' % tsId,
+                               'minSpacing': 0.85,
+                               'peakStorageFraction': 1.0,
+                               'RotationAngle': self.rotationAngle.get(),
+                               'targetNumberOfBeads': self.numberFiducial.get()
+                                }
 
-    def computeStackAlignment(self, tsId):
-        workingFolder = self._getExtraPath(tsId)
-        paramsAlginment = {
-            'input': "%s.st" % tsId,
-            'output': '%s.preali' % tsId,
-            'bin': '%f' % self.binning,
-            'xform': "%s.prexf" % tsId,
-        }
-        argsAlignment = "-input %(input)s " \
-                        "-output %(output)s " \
-                        "-bin %(bin)f " \
-                        "-xform %(xform)s "
-        self.runJob('newstack', argsAlignment % paramsAlginment, cwd=workingFolder)
+            argsAutofidseed = "-TrackCommandFile %(trackCommandFile)s " \
+                              "-MinSpacing %(minSpacing)f " \
+                              "-PeakStorageFraction %(peakStorageFraction)f " \
+                              "-TargetNumberOfBeads %(targetNumberOfBeads)d "
 
-    def _createOutput(self, tomoFn):
-        inputTs = self.inputTiltSeries.get()
-        outTomos = self._createSetOfTomograms()
-        samplingRate = inputTs.getSamplingRate()
+            if self.twoSurfaces.get() == 0:
+                argsAutofidseed += "-TwoSurfaces "
 
-        if self.binning > 1:
-            samplingRate *= self.binning.get()
+            self.runJob('autofidseed', argsAutofidseed % paramsAutofidseed, cwd=workingFolder)
 
-        outTomos.setSamplingRate(samplingRate)
-
-        t = Tomogram(location=tomoFn + ':mrc')
-        t.setObjId(inputTs.getObjId())
-        t.setTsId(inputTs.getTsId())
-        outTomos.append(t)
-
-        self._defineOutputs(outputTomograms=outTomos)
-        self._defineSourceRelation(self.inputTiltSeries, outTomos)
+    def _createOutputStep(self):
+        pass
 
     # --------------------------- UTILS functions ----------------------------
-    def allocateTransformMatrix(self, matrixFile, tiltSeries):
-        frameMatrix = np.empty([3, 3])
-        for image, line in zip(tiltSeries, matrixFile):
-            values = line.split()
-            frameMatrix[0, 0] = float(values(0))
-            frameMatrix[1, 0] = float(values(1))
-            frameMatrix[0, 1] = float(values(2))
-            frameMatrix[1, 1] = float(values(3))
-            frameMatrix[0, 2] = float(values(4))
-            frameMatrix[1, 2] = float(values(5))
-            frameMatrix[3, 0] = 0.0
-            frameMatrix[3, 1] = 0.0
-            frameMatrix[3, 2] = 1.0
-            image.setTransform(matrixFile)
+    def translateTrackCom(self, tsId, paramsDict):
+        trackFileName = tsId + "/" + tsId + "_track.com"
+        trackFilePath = os.path.join(self._getExtraPath(), trackFileName)
 
-    def generateTrackCom(self):
-        template = """
-        # Command file for running BEADTRACK
-        #
-        ####CreatedVersion####4.9.12
-        #
-        # For beads lighter than background, add a line with "LightBeads"
-        #
-        # To restrict tilt alignment to a subset of views, add a line with:
-        # "MaxViewsInAlign #_of_views"
-        #
-        # To exclude views, add a line "SkipViews view_list" with the list of views
-        #
-        # To specify sets of views to be grouped separately in automapping, add a line
-        # "SeparateGroup view_list" with the list of views, one line per group
-        #
-        $beadtrack -StandardInput
-        ImageFile	BBa.preali
-        ImagesAreBinned	1
-        InputSeedModel	BBa.seed
-        OutputModel	BBa.fid
-        PrealignTransformFile	BBa.prexg
-        RotationAngle	-12.5
-        TiltFile	BBa.rawtlt
-        TiltDefaultGrouping	7
-        MagDefaultGrouping	5
-        RotDefaultGrouping	1
-        BeadDiameter	4.95
-        FillGaps	
-        MaxGapSize	5
-        RoundsOfTracking	2
-        #
-        # Set this to 1 to track in local areas
-        LocalAreaTracking	1
-        LocalAreaTargetSize	1000
-        MinBeadsInArea	8
-        MinOverlapBeads	5
-        #
-        # CONTROL PARAMETERS FOR EXPERTS, EXPERIMENTATION, OR SPECIAL CASES
-        #
-        # minimum range of tilt angles for finding axis and for finding tilts
-        MinViewsForTiltalign	4
-        MinTiltRangeToFindAxis	10.0
-        MinTiltRangeToFindAngles	20.0
-        BoxSizeXandY	32,32
-        MaxBeadsToAverage	4
-        # points and minimum for extrapolation
-        PointsToFitMaxAndMin	7,3
-        # fraction of mean, and # of SD below mean: density criterion for rescue
-        DensityRescueFractionAndSD	0.6,1.0
-        # distance criterion for rescue
-        DistanceRescueCriterion	10.0
-        # relaxation of criterion for density and distance rescues
-        RescueRelaxationDensityAndDistance	0.7,0.9
-        # distance for rescue after fit
-        PostFitRescueResidual	2.5
-        # relaxation of density criterion, maximum radius to search
-        DensityRelaxationPostFit	0.9
-        MaxRescueDistance	2.5
-        # Max and min residual changes to use to get mean and SD change
-        ResidualsToAnalyzeMaxAndMin	9,5
-        # minimum residual difference, criterion # of sd's
-        DeletionCriterionMinAndSD	0.04,2.0
-        SobelFilterCentering	
-        $if (-e ./savework) ./savework
-        """
+        if self.importTrackFile == 0:
+            with open(self.trackFilePath.get(), 'r') as f:
+                template = f.read()
+            with open(trackFilePath, 'w') as f:
+                f.write(template)
+        else:
+            template = """# Command file for running BEADTRACK
+#
+####CreatedVersion####4.9.12
+#
+# For beads lighter than background, add a line with "LightBeads"
+#
+# To restrict tilt alignment to a subset of views, add a line with:
+# "MaxViewsInAlign #_of_views"
+#
+# To exclude views, add a line "SkipViews view_list" with the list of views
+#
+# To specify sets of views to be grouped separately in automapping, add a line
+# "SeparateGroup view_list" with the list of views, one line per group
+#
+$beadtrack -StandardInput
+ImageFile	%(tsId)s.st
+ImagesAreBinned	1
+InputSeedModel	%(tsId)s.seed
+OutputModel	%(tsId)s.fid
+RotationAngle	%(rotationAngle)f
+TiltFile	%(tsId)s.rawtlt
+TiltDefaultGrouping	7
+MagDefaultGrouping	5
+RotDefaultGrouping	1
+BeadDiameter	4.95
+FillGaps
+MaxGapSize	5
+RoundsOfTracking	2
+#
+# Set this to 1 to track in local areas
+LocalAreaTracking	1
+LocalAreaTargetSize	1000
+MinBeadsInArea	8
+MinOverlapBeads	5
+#
+# CONTROL PARAMETERS FOR EXPERTS, EXPERIMENTATION, OR SPECIAL CASES
+#
+# minimum range of tilt angles for finding axis and for finding tilts
+MinViewsForTiltalign	4
+MinTiltRangeToFindAxis	10.0
+MinTiltRangeToFindAngles	20.0
+BoxSizeXandY	32,32
+MaxBeadsToAverage	4
+# points and minimum for extrapolation
+PointsToFitMaxAndMin	7,3
+# fraction of mean, and # of SD below mean: density criterion for rescue
+DensityRescueFractionAndSD	0.6,1.0
+# distance criterion for rescue
+DistanceRescueCriterion	10.0
+# relaxation of criterion for density and distance rescues
+RescueRelaxationDensityAndDistance	0.7,0.9
+# distance for rescue after fit
+PostFitRescueResidual	2.5
+# relaxation of density criterion, maximum radius to search
+DensityRelaxationPostFit	0.9
+MaxRescueDistance	2.5
+# Max and min residual changes to use to get mean and SD change
+ResidualsToAnalyzeMaxAndMin	9,5
+# minimum residual difference, criterion # of sd's
+DeletionCriterionMinAndSD	0.04,2.0
+SobelFilterCentering
+$if (-e ./savework) ./savework
+"""
+            with open(trackFilePath, 'w') as f:
+                f.write(template % paramsDict)
