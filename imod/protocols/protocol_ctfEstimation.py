d91c19ac2e659aa6ac48c8c0ac1d9d0e1b10efae
fede-pe <fepe.ig@gmail.com>
2020-10-23 14:41:53 +0200

    discard unestimated ctfs

--- imod/protocols/protocol_ctfEstimation.py
+++ imod/protocols/protocol_ctfEstimation.py
@@ -373,27 +373,29 @@ class ProtImodCtfEstimation(EMProtocol, ProtTomoBase):
         Plugin.runImod(self, 'ctfplotter', argsCtfPlotter % paramsCtfPlotter)
 
     def createOutputStep(self, tsObjId):
-        outputCtfEstimatedSetOfTiltSeries = self.getOutputCtfEstimatedSetOfTiltSeries()
-
         ts = self.inputSetOfTiltSeries.get()[tsObjId]
-        tsId = ts.getTsId()
 
-        newTs = tomoObj.TiltSeries(tsId=tsId)
-        newTs.copyInfo(ts)
-        outputCtfEstimatedSetOfTiltSeries.append(newTs)
+        if os.path.exists(os.path.join(extraPrefix, ts.getFirstItem().parseFileName(extension=".defocus"))):
+            outputCtfEstimatedSetOfTiltSeries = self.getOutputCtfEstimatedSetOfTiltSeries()
 
-        for index, tiltImage in enumerate(ts):
-            newTi = tomoObj.TiltImage()
-            newTi.copyInfo(tiltImage, copyId=True)
-            newTi.setLocation(tiltImage.getLocation())
-            if tiltImage.hasTransform():
-                newTi.setTransform(tiltImage.getTransform())
-            newTs.append(newTi)
+            tsId = ts.getTsId()
 
-        newTs.write(properties=False)
-        outputCtfEstimatedSetOfTiltSeries.update(newTs)
-        outputCtfEstimatedSetOfTiltSeries.write()
-        self._store()
+            newTs = tomoObj.TiltSeries(tsId=tsId)
+            newTs.copyInfo(ts)
+            outputCtfEstimatedSetOfTiltSeries.append(newTs)
+
+            for index, tiltImage in enumerate(ts):
+                newTi = tomoObj.TiltImage()
+                newTi.copyInfo(tiltImage, copyId=True)
+                newTi.setLocation(tiltImage.getLocation())
+                if tiltImage.hasTransform():
+                    newTi.setTransform(tiltImage.getTransform())
+                newTs.append(newTi)
+
+            newTs.write(properties=False)
+            outputCtfEstimatedSetOfTiltSeries.update(newTs)
+            outputCtfEstimatedSetOfTiltSeries.write()
+            self._store()
 
     # --------------------------- UTILS functions ----------------------------
     def getOutputCtfEstimatedSetOfTiltSeries(self):
