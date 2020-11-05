62807fc29e5efd7f1d94deb4fc9018f61c79e7bc
fede-pe <fepe.ig@gmail.com>
2020-11-05 11:31:52 +0100

    remove deprecated code in tsNormalization

--- imod/protocols/protocol_tsNormalization.py
+++ imod/protocols/protocol_tsNormalization.py
@@ -25,8 +25,6 @@
 # **************************************************************************
 
 import os
-import numpy as np
-import imod.utils as utils
 import pyworkflow as pw
 import pyworkflow.protocol.params as params
 import pyworkflow.utils.path as path
