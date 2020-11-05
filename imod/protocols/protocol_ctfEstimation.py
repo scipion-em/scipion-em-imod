afbd0ec70a7e9b3953f9dab07055216116afcbc1
fede-pe <fepe.ig@gmail.com>
2020-11-05 11:36:53 +0100

    remove deprecated code in ctf estimation

--- imod/protocols/protocol_ctfEstimation.py
+++ imod/protocols/protocol_ctfEstimation.py
@@ -25,15 +25,12 @@
 # **************************************************************************
 
 import os
-import numpy as np
-import imod.utils as utils
 import pyworkflow as pw
 import pyworkflow.protocol.params as params
 import pyworkflow.utils.path as path
 from pwem.protocols import EMProtocol
 import tomo.objects as tomoObj
 from tomo.protocols import ProtTomoBase
-from pwem.emlib.image import ImageHandler
 from imod import Plugin
 
 
