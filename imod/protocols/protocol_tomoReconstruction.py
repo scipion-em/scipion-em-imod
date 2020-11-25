5247c5fe4800fb4cc3d6f5902b646d214e367155
fede-pe <fepe.ig@gmail.com>
2020-11-25 11:34:02 +0100

    change in form

--- imod/protocols/protocol_tomoReconstruction.py
+++ imod/protocols/protocol_tomoReconstruction.py
@@ -132,6 +132,20 @@ class ProtImodTomoReconstruction(EMProtocol, ProtTomoBase):
                                         label='Second parameter',
                                         help='Gaussian fall-off parameter')
 
+        form.addHidden(params.USE_GPU,
+                       params.BooleanParam,
+                       default=True,
+                       label="Use GPU for execution",
+                       help="This protocol has both CPU and GPU implementation.\
+                               Select the one you want to use.")
+
+        form.addHidden(params.GPU_LIST,
+                       params.StringParam,
+                       default='0',
+                       expertLevel=params.LEVEL_ADVANCED,
+                       label="Choose GPU IDs",
+                       help="GPU ID. To pick the best available one set 0. For a specific GPU set its number ID.")
+
     # -------------------------- INSERT steps functions ---------------------
     def _insertAllSteps(self):
         for ts in self.inputSetOfTiltSeries.get():
