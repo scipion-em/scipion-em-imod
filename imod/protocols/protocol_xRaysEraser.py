9dd249f4fb4aae1bc8a43b4c69b6c8ee3c7601fd
fede-pe <fepe.ig@gmail.com>
2021-01-26 17:27:06 +0100

    create base protocol

---
+++ imod/protocols/protocol_xRaysEraser.py
@@ -0,0 +1,41 @@
+# **************************************************************************
+# *
+# * Authors:     Federico P. de Isidro Gomez (fp.deisidro@cnb.csi.es) [1]
+# *
+# * [1] Centro Nacional de Biotecnologia, CSIC, Spain
+# *
+# * This program is free software; you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation; either version 2 of the License, or
+# * (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program; if not, write to the Free Software
+# * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+# * 02111-1307  USA
+# *
+# *  All comments concerning this program package may be sent to the
+# *  e-mail address 'scipion@cnb.csic.es'
+# *
+# **************************************************************************
+
+from pwem.protocols import EMProtocol
+from tomo.protocols import ProtTomoBase
+
+
+class ProtImodXcorrPrealignment(EMProtocol, ProtTomoBase):
+    """
+    Erase X-rays, defects, and fiducial markers from aligned tilt-series based on the IMOD procedure.
+    More info:
+            https://bio3d.colorado.edu/imod/doc/man/ccderaser.html
+    """
+
+    _label = 'x-ray eraser'
+
+    def __init__(self, **kwargs):
+        EMProtocol.__init__(self, **kwargs)
