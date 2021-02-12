a7aed7c89c9aa64b94b99a143bd2e79202daaef9
fede-pe <fepe.ig@gmail.com>
2021-02-12 12:01:57 +0100

    fixes in email in description of ALL SCRIPTS

--- imod/protocols/__init__.py
+++ imod/protocols/__init__.py
@@ -1,7 +1,7 @@
 # **************************************************************************
 # *
 # * Authors:     J.M. De la Rosa Trevin (delarosatrevin@scilifelab.se) [1]
-# *              Federico P. de Isidro Gomez (fp.deisidro@cnb.csi.es) [1]
+# *              Federico P. de Isidro Gomez (fp.deisidro@cnb.csic.es) [1]
 # *
 # * [1] SciLifeLab, Stockholm University
 # * [2] Centro Nacional de Biotecnologia, CSIC, Spain
