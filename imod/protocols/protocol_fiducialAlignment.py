71c545dfd63632557576c079a7b4714d5bae8b30
fede-pe <fepe.ig@gmail.com>
2021-03-05 13:24:14 +0100

    fixes in interpolated ts origin

--- imod/protocols/protocol_fiducialAlignment.py
+++ imod/protocols/protocol_fiducialAlignment.py
@@ -670,9 +670,6 @@ class ProtImodFiducialAlignment(EMProtocol, ProtTomoBase):
         x, y, z, _ = ih.getDimensions(newTs.getFirstItem().getFileName())
         newTs.setDim((x, y, z))
 
-        # Update origin after resizing
-        newTs.updateOriginWithResize(1/self.binning.get())
-
         newTs.write(properties=False)
 
         outputInterpolatedSetOfTiltSeries.update(newTs)
