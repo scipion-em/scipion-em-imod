5f9b383d72a38536d5479b77234b08926d3fa185
Grigory Sharov <sharov.grigory@gmail.com>
2022-11-10 12:07:18 +0000

    update changelog

--- imod/__init__.py
+++ imod/__init__.py
@@ -36,7 +36,7 @@ import pwem
 from .constants import IMOD_HOME, ETOMO_CMD, DEFAULT_VERSION, VERSIONS
 
 
-__version__ = '3.0.11'
+__version__ = '3.1'
 _logo = ""
 _references = ['Kremer1996', 'Mastronarde2017']
 
