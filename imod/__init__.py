e9269fe826d08632e5231a55c50f7c60425f05b1
Grigory Sharov <sharov.grigory@gmail.com>
2023-03-12 15:51:25 +0000

    validate input alignment

--- imod/__init__.py
+++ imod/__init__.py
@@ -36,7 +36,7 @@ import pwem
 from .constants import IMOD_HOME, ETOMO_CMD, DEFAULT_VERSION, VERSIONS
 
 
-__version__ = '3.1.6'
+__version__ = '3.1.7'
 _logo = "icon.png"
 _references = ['Kremer1996', 'Mastronarde2017']
 
