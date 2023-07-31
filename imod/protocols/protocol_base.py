7cb9469c32f9df81bbbcc77b0b843f1fa09412ae
fede-pe <fepe.ig@gmail.com>
2023-07-31 13:37:09 +0200

    add residual fino to protocol base

--- imod/protocols/protocol_base.py
+++ imod/protocols/protocol_base.py
@@ -298,6 +298,7 @@ class ProtImodBase(ProtTomoImportFiles, EMProtocol, ProtTomoBase):
 
             outputFiducialModelNoGaps.copyInfo(tiltSeries)
             outputFiducialModelNoGaps.setSetOfTiltSeries(tiltSeriesPointer)
+            outputFiducialModelNoGaps.setHasResidualInfo(True)
 
             outputFiducialModelNoGaps.setStreamState(Set.STREAM_OPEN)
 
@@ -315,6 +316,8 @@ class ProtImodBase(ProtTomoImportFiles, EMProtocol, ProtTomoBase):
 
             outputFiducialModelGaps.copyInfo(self.inputSetOfTiltSeries.get())
             outputFiducialModelGaps.setSetOfTiltSeries(self.inputSetOfTiltSeries)
+            outputFiducialModelGaps.setHasResidualInfo(False)
+
             outputFiducialModelGaps.setStreamState(Set.STREAM_OPEN)
 
 
