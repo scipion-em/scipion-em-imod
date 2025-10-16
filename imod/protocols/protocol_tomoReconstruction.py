076ab0894c1592c8b7321a55a7923361acd22155
JorMaister <jorgejjs86@gmail.com>
2025-10-16 10:38:27 +0200

    Fix tomo rec with even odd

--- imod/protocols/protocol_tomoReconstruction.py
+++ imod/protocols/protocol_tomoReconstruction.py
@@ -287,9 +287,12 @@ class ProtImodTomoReconstruction(ProtImodBase, ProtStreamingBase):
         if tsId not in self.failedItems:
             try:
                 logger.info(cyanStr(f'===> tsId = {tsId}: reconstructing the tomogram...'))
+                with self._lock:
+                    ts = self.getCurrentTs(tsId)
+                outTsFn, outTsOddFn, outTsEvenFn = self.getTmpFileNames(ts)
                 # run tilt
                 paramsTilt = {
-                    "-InputProjections": self.getTmpOutFile(tsId),
+                    "-InputProjections": outTsFn,
                     "-OutputFile": self.getTmpOutFile(tsId, ext=MRC_EXT),
                     "-TILTFILE": self.getExtraOutFile(tsId, ext=TLT_EXT),
                     "-THICKNESS": self.tomoThickness.get(),
@@ -335,11 +338,8 @@ class ProtImodTomoReconstruction(ProtImodBase, ProtStreamingBase):
 
                 oddEvenTmp = [[], []]
                 if self.doOddEven:
-                    with self._lock:
-                        ts = self.getCurrentTs(tsId)
-                        firstTi = ts.getFirstItem()
                     # Odd
-                    paramsTilt['-InputProjections'] = firstTi.getOdd()
+                    paramsTilt['-InputProjections'] = outTsOddFn
                     oddEvenTmp[0] = self.getTmpOutFile(tsId, suffix=ODD, ext=MRC_EXT)
                     paramsTilt['-OutputFile'] = oddEvenTmp[0]
                     self.runProgram(TILT_PROGRAM, paramsTilt)
@@ -349,7 +349,7 @@ class ProtImodTomoReconstruction(ProtImodBase, ProtStreamingBase):
                     Plugin.runImod(self, TRIMVOL_PROGRAM, argsTrimvol % paramsTrimVol)
 
                     # Even
-                    paramsTilt['-InputProjections'] = firstTi.getEven()
+                    paramsTilt['-InputProjections'] = outTsEvenFn
                     oddEvenTmp[1] = self.getTmpOutFile(tsId, suffix=EVEN, ext=MRC_EXT)
                     paramsTilt['-OutputFile'] = oddEvenTmp[1]
                     self.runProgram(TILT_PROGRAM, paramsTilt)
